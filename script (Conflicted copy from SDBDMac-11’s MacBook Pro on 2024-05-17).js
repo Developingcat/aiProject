febad9d3cd4c6e9a7041e831ab8492aa871566e7
Hannah <148793520+Developingcat@users.noreply.github.com>
2024-06-05 13:21:17 -0400

    Uploading final project

---
+++ script (Conflicted copy from SDBDMac-11’s MacBook Pro on 2024-05-17).js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+
+// ******************
+
+/* USE AS REFERENCE */
+
+/*function hamburger() {
+	var menu = document.getElementById("menu-links"); 
+	if (menu.style.display === "block" && logo.style.display === "none") {
+		menu.style.display = "none";
+		logo.style.display ="block";
+	}else {
+		menu.style.display = "block";
+		logo.style.display = "none";
+	}
+
+}  */
+
+ class timelineContent::after {
+	background-color: var(--clr-purple);
+	content: "";
+	position: absolute; 
+	leftL calc(50% - 2px);
+	width: 0.4rem;
+	height: 97%;
+	z-index: -5;
+}
+
+
---
+++ script.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+
+// ******************
+
+/* USE AS REFERENCE */
+
+function hamburger() {
+	var menu = document.getElementById("menu-links"); 
+	if (menu.style.display === "block" && logo.style.display === "none") {
+		menu.style.display = "none";
+		logo.style.display ="block";
+	}else {
+		menu.style.display = "block";
+		logo.style.display = "none";
+	}
+
+} 
+
